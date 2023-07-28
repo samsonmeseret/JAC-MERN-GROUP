85f451f03c87ebfbac51adf8b8ed418bf269df0a
Samson Meseret <samsonmeseretb@gmail.com>
2023-07-28 16:40:44 +0300

    js practice on variables and datatypes

---
+++ js/script.js
@@ -0,0 +1,103 @@
+// alert("JavaScript Connected")
+
+// console.log("first")
+
+// Variable
+// var, let and const 
+
+// Decler
+
+// note: you can re-decler and re-assign it
+var someVar;
+var someVar = "some Var"
+// console.log(someVar)
+
+
+// someVar = "new Value"
+// console.log(someVar)
+// Decler and assign(to a value)
+// var anyName = "MERN Group"
+// console.log(anyName)
+
+// you can't re-decler but you can re-assign
+let car = "BMW"
+car = "TESLA"
+
+// console.log(car)
+
+// both you can't
+const phone = "iphone"
+// phone = "SUMSUNG"
+
+// console.log(phone)
+
+
+var one, two, three;
+
+one=1
+two=2
+three=3
+
+// console.log(one, two, three)
+
+
+
+// DataType
+
+// soem
+// 56
+// false
+
+// 1. Number
+
+let num = -34.0223
+// console.log(typeof num)
+//  2. String
+
+let str = "example 56 dsgfui"
+// console.log(typeof str)
+
+// 3. Boolean (true || false)
+
+const todayIsFriday = true // camel case 
+// camel: someVar
+// pascal: SameVar
+// snake: same_var
+
+// console.log(typeof todayIsFriday)
+
+let nulish = null // javascript bug (mistake)
+
+// console.log(typeof nulish)
+
+var nothing = undefined
+
+
+
+// console.log(typeof nothing)
+
+// Non-primitive data structure:
+
+// Array
+
+// ---------
+// ---------
+
+let arr = ["some", 56, false]
+// console.log(arr[1])
+
+// Object
+
+let obj = {
+    firstName: "Mickle"
+}
+
+
+// console.log( obj["firstName"])
+// console.log( obj.firstName)
+
+
+
+
+
+
