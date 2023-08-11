43e3b76a49fde622ce673805775e974009bb7f49
Samson Meseret <samsonmeseretb@gmail.com>
2023-08-11 16:45:03 +0300

    functions and conditions

--- js/script.js
+++ js/script.js
@@ -190,4 +190,177 @@ const varTwo = 34
 // console.log(2 < "John");
 
 
+// FUNCTIONS
+
+// let num = 30
+// let num2 = 30
+
+//Function declration
+// any function have something to return
+// function adder (){
+//         // console.log(num + num2)
+//         return num + num2
+//         console.log("first")
+// }
+
+// // Invoke || Call
+// console.log(adder())
+// adder()
+
+// ---- PArameter and Argument
+
+function adder(a, b) {
+    //internally how functions declare variable by the name of parameters
+    // var a;
+    // var b;
+
+    console.log(a)
+    console.log(b)
+    // return a + b
+    
+}
+
+//arguments
+// adder(3)
+// console.log(adder(5))
+
+// argument > parameter: reject
+//parameter > argument : for the unspecified param "undefined "will the value
+
+// var someOne;
+// console.log(someOne);
+
+// without regular parameter declration
+
+// function someCase(...some){
+//     console.log(some[0])
+// }
+
+
+// someCase(4,5,6,7,8)
+
+// function rareCase() {
+//     console.log(arguments[0])
+// }
+
+// rareCase(4,5,6,7)
+
+
+// Function Declration  ways
+
+// traditional
+// function declaration
+function name(params) {
+    
+}
+//function expression
+// let tradFunExp = function (){  // annonamus functions
+// console.log("test")
+// }
+// tradFunExp()
+
+// ES6 (2016) --> Arrow Function
+
+//function expression
+// let arrowFunction = (x, y) => {
+//     console.log(x + y);
+// }
+//one line return on arrow functions
+// let arrowFunction = (x, y) => console.log(x + y);
+
+// arrowFunction(5, 5)
+
+
+// CONDITIONS
+
+// IF ELSE CONDITIONS
+// function paly(isFreeTime) {
+
+//     if(isFreeTime){
+//         console.log("i will play")
+//     }else{
+//         console.log("i will work || study ")
+//     }
+    
+// }
+
+// paly(3>2)
+
+// function gradeLetter(grade) {
+//     if(grade>90){
+//         console.log("grade: A")
+//     }else if(grade>80){
+//         console.log("grade: B")
+//     }else if(grade> 70){
+//         console.log("grade: B-")
+//     }else{
+//         console.log("grade: C")
+//     }
+// }
+
+// gradeLetter(50)
+
+// SWITCH
+
+// function play (isFreeTime=true){
+
+//     switch (isFreeTime) {
+//         case true:
+//             console.log("i will play")
+//             break;
+//         case false:
+//             console.log("i will study")
+//             break
+//         default:
+//             console.log("i don't know")
+//             break;
+//     }
+
+
+// }
+
+// play("zimbiye")
+
+// function textBased(text) {
+//     switch (text) {
+//         case "hi":
+//             console.log("Hello there")
+//             break;
+//         case "bye":
+//             console.log("have a good day/night")
+//             break
+//         default:
+//             break;
+//     }
+    
+// }
+
+// textBased("bye")
+
+
+// MOdern OPerators
+
+// Ternary Operator
+
+// function paly(isFreeTime) {
+
+//     return isFreeTime? console.log("i will play") : console.log("i will study")
+    
+// }
+
+
+// console.log(paly(false))
+
+
+// False || True
+
+// function today(isMonday) {
+
+//     isMonday && console.log("today is monday, my worest day !")
+    
+// }
+
+// today(false)
+
+
 
